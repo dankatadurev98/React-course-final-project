e3173b81f8c43517bba5ce9101d8aa593412916c
dankatadurev98 <dankatadurev@gmail.com>
2025-11-23 14:57:38 +0200

    Add Game working

--- client/src/components/addGame/AddGame.jsx
+++ client/src/components/addGame/AddGame.jsx
@@ -1,7 +1,49 @@
+import { useNavigate } from "react-router"
+
 export default function AddGame() {
+
+  let navigator = useNavigate();
+
+  function onSubmitForm(formData) {
+
+    
+
+    let data = Object.fromEntries(formData)
+
+    let fields = {
+      title: data.title,
+      imageUrl: data.imageUrl,
+      price: data.price,
+      genre: data.genre,
+      date: data.date,
+      summary: data.summary,
+    }
+
+    
+
+      fetch('http://localhost:3030/jsonstore/games',{
+        method:'POST',
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(fields)
+      })
+      .then(res=>
+        res.json()
+      )
+      .then(data=>{
+        console.log(`Correct Post Request!`)
+        navigator('/catalog')
+      })
+      .catch(info=>{
+        console.log(`Problem with Post Request!`)
+      })
+    
+
+   
+  }
+
   return (
     <div className="bg-gray-900 min-h-screen flex items-center justify-center px-6">
-      <form className="bg-gray-800 p-8 rounded-xl shadow-lg w-full max-w-xl">
+      <form className="bg-gray-800 p-8 rounded-xl shadow-lg w-full max-w-xl" action={onSubmitForm}>
 
         <h2 className="text-3xl font-extrabold text-center mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-600">
           Add New Game
@@ -12,6 +54,14 @@ export default function AddGame() {
         <input
           type="text"
           name="title"
+          className="w-full p-3 rounded-md bg-gray-700 text-white mb-4 focus:outline-none focus:ring focus:ring-purple-500"
+        />
+
+        {/* GENRE */}
+        <label className="block text-purple-300 mb-2">Genre:</label>
+        <input
+          type="text"
+          name="genre"
           className="w-full p-3 rounded-md bg-gray-700 text-white mb-4 focus:outline-none focus:ring focus:ring-purple-500"
         />
 
@@ -28,14 +78,6 @@ export default function AddGame() {
         <input
           type="number"
           name="price"
-          className="w-full p-3 rounded-md bg-gray-700 text-white mb-4 focus:outline-none focus:ring focus:ring-purple-500"
-        />
-
-        {/* GENRE */}
-        <label className="block text-purple-300 mb-2">Genre:</label>
-        <input
-          type="text"
-          name="genre"
           className="w-full p-3 rounded-md bg-gray-700 text-white mb-4 focus:outline-none focus:ring focus:ring-purple-500"
         />
 
